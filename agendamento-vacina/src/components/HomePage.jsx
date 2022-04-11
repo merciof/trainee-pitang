0735b89aa16946f9cc8414debc5f481d73a7490d
merciof <merciofilho@gmail.com>
2022-04-11 16:41:51 -0300

    remove unused import

--- agendamento-vacina/src/components/HomePage.jsx
+++ agendamento-vacina/src/components/HomePage.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import AppBar from "@material-ui/core/AppBar";
 import { Footer } from "./Footer.jsx";
 import { Navbar } from "./Navbar.jsx";
-
 
 export function HomePage({ children }) {
   return (
     <React.Fragment>
-      <Navbar/>
+      <Navbar />
       <main>{children}</main>
-  
       <Footer text={"agendamento Vacina"} />
     </React.Fragment>
   );
