d3202fb958c037b389742eed8dab9ac5eaa7c886
merciof <merciofilho@gmail.com>
2022-04-11 16:46:06 -0300

    remove children prop

--- agendamento-vacina/src/components/HomePage.jsx
+++ agendamento-vacina/src/components/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Footer } from "./Footer.jsx";
+import { Hero } from "./Hero.jsx";
 import { Navbar } from "./Navbar.jsx";
 
-export function HomePage({ children }) {
+export function HomePage() {
   return (
     <React.Fragment>
       <Navbar />
-      <main>{children}</main>
+      <Hero />
       <Footer text={"agendamento Vacina"} />
     </React.Fragment>
   );
