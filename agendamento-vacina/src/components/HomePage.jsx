0b1c05d2cc7de36dc90db88145b573f8aebaa64f
merciof <merciofilho@gmail.com>
2022-04-11 16:41:01 -0300

    HomePage subdivision into components

--- agendamento-vacina/src/components/HomePage.jsx
+++ agendamento-vacina/src/components/HomePage.jsx
@@ -1,90 +1,16 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
+import { Footer } from "./Footer.jsx";
+import { Navbar } from "./Navbar.jsx";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      <Link color="inherit" href="https://mui.com/">
-        Agendamento Vacina
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
-
-export function HomePage() {
-  const classes = useStyles();
+export function HomePage({ children }) {
   return (
     <React.Fragment>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Agendamento Vacina
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        {/* Hero */}
-        <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography
-              variant="h5"
-              align="center"
-              color="textSecondary"
-              paragraph
-            >
-              Marque e acompanhe o seu agendamento
-            </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justifyContent="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Agendar
-                  </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Ver Agendamentos
-                  </Button>
-                </Grid>
-              </Grid>
-            </div>
-          </Container>
-        </div>
-        {/* End hero */}
-      </main>
-      {/* Footer */}
-      <footer className={classes.footer}>
-        <Copyright />
-      </footer>
-      {/* End footer */}
+      <Navbar/>
+      <main>{children}</main>
+  
+      <Footer text={"agendamento Vacina"} />
     </React.Fragment>
   );
 }
