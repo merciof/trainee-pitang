21088e74201271665671ba765abf3d52aa182866
merciof <merciofilho@gmail.com>
2022-04-13 16:50:43 -0300

    create component BirthDayPicker

---
+++ agendamento-vacina/src/components/BirthDayDatePicker.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+import { getMonth, getYear } from "date-fns";
+import range from "lodash/range";
+import { registerLocale } from "react-datepicker";
+
+import { ptBR } from "date-fns/locale";
+
+registerLocale("ptBR", ptBR);
+
+export function BirthdayDatePicker({ startDate, setStartDate }) {
+
+  const years = range(1920, getYear(new Date()) + 1, 1);
+
+  const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+  return (
+    <DatePicker
+      locale={"ptBR"}
+      renderCustomHeader={({
+        date,
+        changeYear,
+        changeMonth,
+        decreaseMonth,
+        increaseMonth,
+        prevMonthButtonDisabled,
+        nextMonthButtonDisabled,
+      }) => (
+        <div
+          style={{
+            margin: 10,
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
+            {"<"}
+          </button>
+          <select
+            value={getYear(date)}
+            onChange={({ target: { value } }) => changeYear(value)}
+          >
+            {years.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+
+          <select
+            value={months[getMonth(date)]}
+            onChange={({ target: { value } }) =>
+              changeMonth(months.indexOf(value))
+            }
+          >
+            {months.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+
+          <button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
+            {">"}
+          </button>
+        </div>
+      )}
+      selected={startDate}
+      onChange={(date) => setStartDate(date)}
+    />
+  );
+}
