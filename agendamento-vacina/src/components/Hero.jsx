5e25a06506e5bba1cdd6b34d5052e8acf0fa755f
merciof <merciofilho@gmail.com>
2022-04-17 22:34:47 -0300

    add links

--- agendamento-vacina/src/components/Hero.jsx
+++ agendamento-vacina/src/components/Hero.jsx
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   heroContent: {
@@ -15,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export function Hero({majorText, leftButtonText, rightButtonText}) {
+export function Hero({ majorText, leftButtonText, rightButtonText }) {
   const classes = useStyles();
 
   return (
@@ -27,14 +28,21 @@ export function Hero({majorText, leftButtonText, rightButtonText}) {
         <div className={classes.heroButtons}>
           <Grid container spacing={2} justifyContent="center">
             <Grid item>
-              <Button variant="contained" color="primary">
-                {leftButtonText}
-              </Button>
+              <Link
+                to={"/createAppointment"}
+                style={{ textDecoration: "none" }}
+              >
+                <Button variant="contained" color="primary">
+                  {leftButtonText}
+                </Button>
+              </Link>
             </Grid>
             <Grid item>
-              <Button variant="outlined" color="primary">
-                {rightButtonText}
-              </Button>
+              <Link to={"/appointmentList"} style={{ textDecoration: "none" }}>
+                <Button variant="outlined" color="primary">
+                  {rightButtonText}
+                </Button>
+              </Link>
             </Grid>
           </Grid>
         </div>
