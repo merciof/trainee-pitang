ee1970dd8e775c7a7f4ac5e1db65160c460442bc
merciof <merciofilho@gmail.com>
2022-04-18 23:53:44 -0300

    add custom methods

--- agendamento-vacina/src/service/appointmentService.js
+++ agendamento-vacina/src/service/appointmentService.js
@@ -1,19 +1,25 @@
 import axios from "axios";
 
-const URL = "http://localhost:8080/api/appointment/";
+const API_URL = "http://localhost:8080/api/appointment/";
 
 const appointmentService = {
   index: function () {
-    return axios.get("http://localhost:8080/api/appointment/");
+    return axios.get(API_URL);
   },
   create: function (body) {
-    return axios.post("http://localhost:8080/api/appointment/", body);
+    return axios.post(API_URL, body);
   },
   read: function (id) {
-    return axios.get("http://localhost:8080/api/appointment/" + id);
+    return axios.get(API_URL + id);
   },
   getAppointmentsByMonth: function (body) {
     return axios.post("http://localhost:8080/api/getAppointmentsByMonth", body);
+  },
+  getAppointmentsByDay: function (body) {
+    return axios.post("http://localhost:8080/api/getAppointmentsByDay", body);
+  },
+  getAppointmentsByHour: function (body) {
+    return axios.post("http://localhost:8080/api/getAppointmentsByHour", body);
   },
 };
 
