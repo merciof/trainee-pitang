2aa0144371e5350fc94f82763f6c5193793b555f
merciof <merciofilho@gmail.com>
2022-04-20 10:25:48 -0300

    refactor folder structure

---
+++ agendamento-vacina/src/services/appointmentService/appointmentService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/appointment/";
+
+const appointmentService = {
+  index: function () {
+    return axios.get(API_URL);
+  },
+  create: function (body) {
+    return axios.post(API_URL, body);
+  },
+  read: function (id) {
+    return axios.get(API_URL + id);
+  },
+  put: function (body) {
+    return axios.put(API_URL, body);
+  },
+  getAppointmentsByMonth: function (body) {
+    return axios.post("http://localhost:8080/api/getAppointmentsByMonth", body);
+  },
+  getAppointmentsByDay: function (body) {
+    return axios.post("http://localhost:8080/api/getAppointmentsByDay", body);
+  },
+  getAppointmentsByHour: function (body) {
+    return axios.post("http://localhost:8080/api/getAppointmentsByHour", body);
+  },
+};
+
+export { appointmentService };
